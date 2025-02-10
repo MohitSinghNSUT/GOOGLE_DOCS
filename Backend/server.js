7e7321231fd3a18a31e7a0ac65bfabbf91dbb575
MohitSinghNSUT <118382421+MohitSinghNSUT@users.noreply.github.com>
2025-02-10 10:00:49 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://gilded-sherbet-4bea21.netlify.app",
   methods: ["GET", "POST"],
   credentials: true,
 };
