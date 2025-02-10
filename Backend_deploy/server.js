6dcf0f1ebab3dca99b61a17dea788eabfaf6985e
MohitSinghNSUT <118382421+MohitSinghNSUT@users.noreply.github.com>
2025-02-10 10:39:44 +0530

    Add files via upload

---
+++ Backend_deploy/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+const http = require("http");
+const Server = socketio.Server;
+const cors = require("cors");
+const path=require('path');
+const db = require("./database");
+const document = require("./schema");
+const server = http.createServer(app);
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html")); // Serve an HTML file from the 'public' folder
+});
+app.get("/documents/:id",(req,res)=>{
+  console.log("enter ",req.params);
+  res.sendFile(path.join(__dirname, "public", "index.html")); // Serve an HTML file from the 'public' folder
+})
+const corsOption = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+  credentials: true,
+};
+const io = new Server(server, {
+  cors: corsOption,
+});
+app.use(cors(corsOption));
+io.on("connection", (socket) => {
+  console.log("connection");
+  socket.on("get-document", async(id) => {
+    const inserted= await findOrCreate(id);
+    socket.join(id);
+    socket.emit("load-document", inserted.data);
+    socket.on("send-change", (delta) => {
+      socket.broadcast.to(id).emit("receive-changes", delta);
+    });
+    socket.on("save-document",async (doc)=>{
+       const update=await document.findByIdAndUpdate(id,{data:doc});
+    });
+  });
+});
+const findOrCreate = async (id) => {
+  if (!id) return;
+  const findAns = await document.findById(id);
+  if (findAns) return findAns;
+  return document.create({ _id: id, data: "" });
+};
+server.listen(3000, () => {
+  console.log("server started");
+});
